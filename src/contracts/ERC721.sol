6dd80941f17b90c6b0584b93751e7ceb2ec2412d
Neil Sukalikar <neil.sukalikar@gmail.com>
2022-05-05 19:12:07 +0530

    ERC721Enumerable interface integration

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -48,7 +48,7 @@ contract ERC721 {
         return owner != address(0);
     }
 
-    function _mint(address to, uint256 tokenId) internal {
+    function _mint(address to, uint256 tokenId) internal virtual {
         // requires that the address is not zero
         require(to != address(0), 'ERC721: minting to the zero address');
         // require that the token does not exisist

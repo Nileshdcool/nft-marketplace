0434826f0893874010d12cd422b45b8869fd2549
Neil Sukalikar <neil.sukalikar@gmail.com>
2022-05-06 18:49:38 +0530

    Enumerable implementation

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -8,37 +8,50 @@ contract ERC721Enumerable is ERC721 {
     uint256[] private _allTokens;
 
     //mapping from tokenId to position in _allTokens array
+    // 1 => 1 | 2=>2
+    mapping(uint256 => uint256) private _allTokensIndex;
     
     // mapping of owner to list of all owner token ids
+    // #232324 => [1,5,6]
+    mapping(address => uint256[]) private _ownedTokens;
 
     // mapping from token ID index of the owner tokens list
-
-    /// @notice Count NFTs tracked by this contract
-    /// @return A count of valid NFTs tracked by this contract, where each one of
-    ///  them has an assigned and queryable owner not equal to the zero address
-    function totalSupply() external view returns (uint256) {
-        return _allTokens.length;
-    }
-
-    /// @notice Enumerate valid NFTs
-    /// @dev Throws if `_index` >= `totalSupply()`.
-    /// @param _index A counter less than `totalSupply()`
-    /// @return The token identifier for the `_index`th NFT,
-    ///  (sort order not specified)
-    function tokenByIndex(uint256 _index) external view returns (uint256);
-
-    /// @notice Enumerate NFTs assigned to an owner
-    /// @dev Throws if `_index` >= `balanceOf(_owner)` or if
-    ///  `_owner` is the zero address, representing invalid NFTs.
-    /// @param _owner An address where we are interested in NFTs owned by them
-    /// @param _index A counter less than `balanceOf(_owner)`
-    /// @return The token identifier for the `_index`th NFT assigned to `_owner`,
-    ///   (sort order not specified)
-    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
+    // 1 => 
+    mapping(uint256 => uint256) private _ownedTokensIndex;
 
     function _mint(address to, uint256 tokenId) internal override(ERC721)
     {
         super._mint(to,tokenId);
-        
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokensToOwnerEnumeration(to,tokenId);
+    }
+
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
+        _allTokens.push(tokenId);
+    } 
+
+    function _addTokensToOwnerEnumeration(address to, uint256 tokenId) private
+    {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function tokenByIndex(uint256 index) public view returns(uint256) {
+        // index should not be out of bounds
+        require(index < totalSupply(),'global index is out of bounds');
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint index) public view returns(uint256) {
+        require(index < balanceOf(owner),'global index is out of bounds');
+        return _ownedTokens[owner][index];
+    }
+
+    /// @notice Count NFTs tracked by this contract
+    /// @return A count of valid NFTs tracked by this contract, where each one of
+    ///  them has an assigned and queryable owner not equal to the zero address
+    function totalSupply() public view returns (uint256) {
+        return _allTokens.length;
     }
 }
